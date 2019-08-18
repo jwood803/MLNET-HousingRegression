ba68dbefabe3be132f5c8c877b2a9b55a100c4be
J W <woodjon@gmail.com>
2019-08-18 11:09:35 -0400

    Add housing project

---
+++ HouseRegression/HouseRegression/HousingPrediction.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HouseRegression
+{
+    public class HousingPrediction
+    {
+        [ColumnName("Score")]
+        public float PredictedHouseValue { get; set; }
+    }
+}
---
+++ HouseRegression/HouseRegression/Program.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.ML;
+using System;
+using System.Linq;
+
+namespace HouseRegression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new MLContext();
+
+            var data = context.Data.LoadFromTextFile<HousingData>("./housing.csv", hasHeader: true, separatorChar: ',');
+
+            var preview = data.Preview();
+
+            var features = data.Schema
+                .Select(col => col.Name)
+                .Where(colName => colName != "Label" && colName != "OceanProximity")
+                .ToArray();
+
+            var dataPrepPipeline = context.Transforms.Text.FeaturizeText("Text", "OceanProximity")
+                .Append(context.Transforms.Concatenate("Features", features));
+
+            var preppedData = dataPrepPipeline.Fit(data);
+
+            var trainer = context.Regression.Trainers.LbfgsPoissonRegression();
+
+            var pipeline = dataPrepPipeline.Append(trainer);
+
+            var model = pipeline.Fit(data);
+
+            // Save full model
+            context.Model.Save(model, data.Schema, "./housing-model.zip");
+
+            // Save data prep
+            context.Model.Save(preppedData, data.Schema, "./housing-data-prep.zip");
+
+            // Save trainer
+            var transformedData = preppedData.Transform(data);
+            var transformedTrainer = trainer.Fit(transformedData);
+            context.Model.Save(transformedTrainer, transformedData.Schema, "./housing-trainer.zip");
+
+            var predictionFunc = context.Model.CreatePredictionEngine<HousingData, HousingPrediction>(model);
+
+            var prediction = predictionFunc.Predict(new HousingData
+            {
+                Longitude = -122.25f,
+                Latitude = 37.85f,
+                HousingMedianAge = 55.0f,
+                TotalRooms = 1627.0f,
+                TotalBedrooms = 235.0f,
+                Population = 322.0f,
+                Households = 120.0f,
+                MedianIncome = 8.3014f,
+                OceanProximity = "NEAR BAY"
+            });
+        }
+    }
+}
